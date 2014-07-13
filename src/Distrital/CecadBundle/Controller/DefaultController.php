539e0a86766e1089ea8d74e1a209f32ff3711a7c
Hector Dussan <hedussan@hotmail.com>
2014-07-12 21:16:10 -0500

    Se implementa login y registro de usuarios en el sistema

--- src/Distrital/CecadBundle/Controller/DefaultController.php
+++ src/Distrital/CecadBundle/Controller/DefaultController.php
@@ -3,75 +3,85 @@
 namespace Distrital\CecadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
-
-use Distrital\CecadBundle\Entity\Director;
-use Distrital\CecadBundle\Entity\EquipoProyecto;
+use Distrital\CecadBundle\Entity\Usuario;
+use Distrital\CecadBundle\Form\UsuarioType;
 
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-    
-    	$r_director = $this->getDoctrine()->getRepository('DistritalCecadBundle:Director');
-    	$r_equipoProyecto = $this->getDoctrine()->getRepository('DistritalCecadBundle:EquipoProyecto');
-
-    	
     	$request = $this->getRequest(); 
-    	$accion = $request->get("accion");
-    	
-    	
-    	if (!is_null($accion)){
-			if (strcmp($accion, "agregar")==0){
-				$codigo = $request->get("codigo");
-				$directorNuevo = new Director();
-				$directorNuevo->setCodDirector($codigo);
-				
-				
-		        $em = $this->getDoctrine()->getManager();
-		        $em->persist($directorNuevo);
-		        $em->flush();
-			}
-			if (strcmp($accion, "agregarEquipoProyecto")==0){
-				$fecha_ini = $request->get("fecha_ini");
-				$estado_equ_pro = $request->get("estado_equ_pro");
-				$codigo = $request->get("codigo");
-				
-				$director = $r_director->findOneByCodDirector($codigo);
-				
-				
-				
-				$equipoNuevo = new EquipoProyecto();
-				
-				$equipoNuevo->setFechaIni(new \Datetime($fecha_ini));
-				$equipoNuevo->setEstadoEquPro($estado_equ_pro);
-				$equipoNuevo->setDirector($director);
-				
-				
-		        $em = $this->getDoctrine()->getManager();
-		        $em->persist($equipoNuevo);
-		        $em->flush();
-			}
-    	}
-    
-
-
-		$directores = $r_director->findAll();
-		
-		$equiposProyecto = $r_equipoProyecto->findAll();
-
-		
-		
+	
 
         return $this->render('DistritalCecadBundle:Default:index.html.twig', 
         		array(
-        			'directores'=>$directores,
-        			'equiposProyecto' => $equiposProyecto,
         		
         		));
     }
+	public function registroAction()
+	{
+		$request = $this->get('request');
+		$entity = new Usuario();
+        $form = $this->createForm(new UsuarioType(), $entity, array(
+            'action' => $this->generateUrl('registro'),
+            'method' => 'POST',
+        ));
 
+        $form->add('submit', 'submit', array('label' => 'Registrarse'));
+        
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($entity);
+			$password = $encoder->encodePassword($entity->getPassword(), "");
+			$entity->setClave($password);
+			$entity->setSalt("");
+			$entity->setEstado("A");
+            
+            $em->persist($entity);
+            $em->flush();
+            
+            //ToDo: Colocar en la bolsa el mensaje sobre el registro
+
+            return $this->redirect($this->generateUrl('distrital_cecad_homepage'));
+        }
+
+        return $this->render('DistritalCecadBundle:Default:registro.html.twig', array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ));
+
+    }
+
+    public function loginAction()
+    {
+		$request = $this->getRequest();
+		$session = $request->getSession();
+		// get the login error if there is one
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+			$error = $request->attributes->get(
+						SecurityContext::AUTHENTICATION_ERROR
+					);
+		} else {
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
+
+		return $this->render(
+				'DistritalCecadBundle:Default:login.html.twig',
+				array(
+					// last username entered by the user
+					'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+					'error'=> $error,
+				));
+	}
 
 }
 
