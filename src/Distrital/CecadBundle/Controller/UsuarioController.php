98abe375594ffddc4452f6f30ce9933bbd265258
Hector Dussan <hedussan@hotmail.com>
2014-07-12 21:18:48 -0500

    Se implementa UsuarioController con la relación con Proyectos

---
+++ src/Distrital/CecadBundle/Controller/UsuarioController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Distrital\CecadBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+
+use Distrital\CecadBundle\Entity\Usuario;
+use Distrital\CecadBundle\Form\UsuarioType;
+
+
+/**
+* Usuario controller.
+*
+* @Route("/usuario")
+*/
+class UsuarioController extends Controller
+{
+	/**
+	* Displays a form to create a new Inmueble entity.
+	*
+	* @Route("/", name="usuario_index")
+	* @Method("GET")
+	* @Template()
+	*/
+    public function indexAction()
+    {
+    
+		$em = $this->getDoctrine()->getManager();
+
+		$usuario = $this->getUser();
+
+		$id = 0;
+		if (!isset($usuario)){
+			return $this->redirect($this->generateUrl('distrital_cecad_homepage'));
+		}
+		
+		$em = $this->getDoctrine()->getManager();
+
+		$r_participantes = $em->getRepository('DistritalCecadBundle:ParticipanteProyecto');
+		$participaciones = $r_participantes->findByUsuario($usuario);
+		
+		
+		
+        return $this->render('DistritalCecadBundle:Usuario:index.html.twig', 
+        		array(
+					'usuario'=>$usuario,
+        			'participaciones'=>$participaciones,
+        		));
+    }
+}
