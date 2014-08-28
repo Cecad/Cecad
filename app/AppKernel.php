3fb0d004b44640df924f12fb576ca2b24f5cb85d
Jorge Gonzalez <jag2kn@gmail.com>
2014-08-28 16:08:48 -0500

    Se agrega Fixtures e IDETools

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Distrital\CecadBundle\DistritalCecadBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new IDE\ToolsBundle\IDEToolsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
