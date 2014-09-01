284fdcb2da8f02ec2a597f1fa0d61abfffdd211c
Hector Dussan <hedussan@hotmail.com>
2014-08-31 19:19:57 -0500

    Se agrega librerias para manejo de estados unificado

---
+++ src/Distrital/CecadBundle/Librerias/Estado.php
@@ -0,0 +1,13 @@
+<?php
+// src/Distrital/CecadBundle/Librerias/Estado.php
+namespace Distrital\CecadBundle\Librerias;
+
+class Estado
+{
+
+    const SOLICITADO	=	'S';
+	const APROBADO		=	'A';
+	const TERMINADO		=	'T';
+	const CANCELADO		=	'C';
+
+}
